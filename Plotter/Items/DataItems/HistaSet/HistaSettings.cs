b55de7f8458a79b0391496069bd5d3f05215eb17
vlrblkrv <belokurov.valeriy@gmail.com>
2016-10-10 14:06:36 +0300

    blablabla

---
+++ Plotter/Items/DataItems/HistaSet/HistaSettings.cs
@@ -0,0 +1,30 @@
+﻿namespace Plotter.Items.DataItems.HistaSet
+{
+    /// <summary>
+    /// Settings of histagram.
+    /// </summary>
+    internal class HistaSettings
+    {
+        /// <summary>
+        /// Count of baskets.
+        /// </summary>
+        public int BasketCount { get; }
+
+        /// <summary>
+        /// Size of each basket.
+        /// </summary>
+        public float BasketDelta { get; }
+
+        /// <summary>
+        /// Position of front border of the first basket.
+        /// </summary>
+        public float DataStart { get; }
+
+        public HistaSettings(int basketCount, float basketDelta, float dataStart)
+        {
+            BasketCount = basketCount;
+            BasketDelta = basketDelta;
+            DataStart = dataStart;
+        }
+    }
+}
---
+++ Plotter/Items/DataItems/HistaSet/Histagram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Plotter.Items.DataItems.HistaSet
+{
+    internal class Histagram
+    {
+        private IEnumerable<float> _elements;
+
+        private Histagram(IEnumerable<float> elements)
+        {
+            _elements = elements;
+        }
+
+        /// <summary>
+        /// Init histagram by data-seq.
+        /// </summary>
+        public static Histagram Init(IEnumerable<float> elements) => new Histagram(elements);
+
+        /// <summary>
+        /// Processing data-seq: calc basket contents.
+        /// </summary>
+        public void Process()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
